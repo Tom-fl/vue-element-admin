30e9a17899027aeb9d7946a28fb52ec323d676ef
Tom-fl <1399936889@qq.com>
2022-03-08 14:14:48 +0800

    init

---
+++ src/layout/components/AppMain.vue
@@ -0,0 +1,68 @@
+<!--
+ * @Author: Tom
+ * @LastEditors: Tom
+ * @Date: 2022-03-01 17:16:34
+ * @LastEditTime: 2022-03-03 17:49:54
+ * @Email: str-liang@outlook.com
+ * @FilePath: \vue-element-admin\src\layout\components\AppMain.vue
+ * @Environment: Win 10
+ * @Description:
+-->
+<template>
+  <section class="app-main">
+    <transition name="fade-transform" mode="out-in">
+      <keep-alive :include="cachedViews">
+        <router-view :key="key" />
+      </keep-alive>
+    </transition>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AppMain",
+  computed: {
+    cachedViews() {
+      console.log(this.$store.state.tagsView.cachedViews);
+      return this.$store.state.tagsView.cachedViews;
+    },
+    key() {
+      return this.$route.path;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.app-main {
+  /* 50= navbar  50  */
+  min-height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+}
+
+.fixed-header + .app-main {
+  padding-top: 50px;
+}
+
+.hasTagsView {
+  .app-main {
+    /* 84 = navbar + tags-view = 50 + 34 */
+    min-height: calc(100vh - 84px);
+  }
+
+  .fixed-header + .app-main {
+    padding-top: 84px;
+  }
+}
+</style>
+
+<style lang="scss">
+// fix css style bug in open el-dialog
+.el-popup-parent--hidden {
+  .fixed-header {
+    padding-right: 15px;
+  }
+}
+</style>
